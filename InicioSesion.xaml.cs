e44b2c7e82a101987c4ae732a234cf32c49a967b
UrielCendon <urielcd2013@gmail.com>
2025-09-24 00:34:34 -0600

    Pictionary Musical Cliente, Añade Ventanas de Inicio de Sesión, Crear Cuenta, Ventana Principal y relacionadas

---
+++ InicioSesion.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PictionaryMusicalCliente
+{
+    /// <summary>
+    /// Lógica de interacción para InicioSesion.xaml
+    /// </summary>
+    public partial class InicioSesion : Window
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void CuadroCombinadoSeleccionLenguaje(object sender, SelectionChangedEventArgs e)
+        {
+            if (cuadroCombinadoLenguaje.SelectedIndex == 0)
+                Properties.Settings.Default.idiomaCodigo = "es-MX";
+            else
+                Properties.Settings.Default.idiomaCodigo = "en-US";
+            Properties.Settings.Default.Save();
+
+            PictionaryMusicalCliente.Properties.Langs.Lang.Culture = new CultureInfo(Properties.Settings.Default.idiomaCodigo);
+
+            InicioSesion nuevoInicioSesion = new InicioSesion();
+            Application.Current.MainWindow = nuevoInicioSesion;
+            nuevoInicioSesion.Show();
+            this.Close();
+        }
+
+        private void BotonEntrar(object sender, RoutedEventArgs e)
+        {
+            VentanaPrincipal ventana = new VentanaPrincipal();
+            ventana.Show();
+            this.Close();
+        }
+
+        private void BotonEntrarInvitado(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BotonCrearCuenta(object sender, RoutedEventArgs e)
+        {
+            CrearCuenta ventana = new CrearCuenta();
+            ventana.ShowDialog();
+        }
+    }
+}
