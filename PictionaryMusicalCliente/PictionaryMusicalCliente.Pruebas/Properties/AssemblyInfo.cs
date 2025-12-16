f44ce0144c32098d883f957c33c4929c69a1c623
UrielCendon <urielcd2013@gmail.com>
2025-12-15 23:08:07 -0600

    Proyecto de pruebas del cliente

---
+++ PictionaryMusicalCliente/PictionaryMusicalCliente.Pruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PictionaryMusicalCliente.Pruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PictionaryMusicalCliente.Pruebas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("734cf5f5-cc5a-4cc1-88db-0bdc838b21ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
